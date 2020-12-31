1a13efc482364ff788452cde516816037b68949d
Sean Gibson <seanthomasgibson@gmail.com>
2020-12-31 10:21:30 -0800

    Displaying facts on multiple numbers. Modified number_facts/number_facts.html and number_facts/number_facts.js.

--- number_facts/number_facts.js
+++ number_facts/number_facts.js
@@ -1,7 +1,11 @@
 const BASE_URL = "http://numbersapi.com/";
 const FAVORITE_NUM = 7;
 
-// get json from Numbers API
+/**
+ * Gets a fact on num
+ * @param {number} num The number to get a fact on
+ * @return {string} The fact, or an error message
+ */
 async function getFact(num) {
   try {
     const resp = await axios.get(`${BASE_URL}${num}?json`);
@@ -12,7 +16,29 @@ async function getFact(num) {
   }
 }
 
+/**
+ * Create an HTML list item of fact
+ * @param {string} fact The fact to create a list item from
+ * @return {string} The HTML list item
+ */
+function generateFact(fact) {
+  return `<li>${fact}</li>`;
+}
+
 $(async function() {
+  // get json from Numbers API
   const fact = await getFact(FAVORITE_NUM);
   console.log(fact);
+
+  // get data on mult numbers
+  try {
+    const resp = await axios.get(`${BASE_URL}${FAVORITE_NUM},42?json`);
+    facts = resp.data;
+    for (let num in facts) {
+      $("#mult-num-facts").append($(generateFact(facts[num])))
+    }
+  }
+  catch(e) {
+    console.log(e);
+  }
 });
