d397d8cd0450cf7639c779e60f76844bf697eed8
Sean Gibson <seanthomasgibson@gmail.com>
2020-12-31 10:12:13 -0800

    Added jQuery and Axios, and getting 1 fact. Modified number_facts/number_facts.html and number_facts/number_facts.js.

--- number_facts/number_facts.js
+++ number_facts/number_facts.js
@@ -1,1 +1,18 @@
-console.log("Working");
+const BASE_URL = "http://numbersapi.com/";
+const FAVORITE_NUM = 7;
+
+// get json from Numbers API
+async function getFact(num) {
+  try {
+    const resp = await axios.get(`${BASE_URL}${num}?json`);
+    return resp.data.text;
+  }
+  catch(e) {
+    return e;
+  }
+}
+
+$(async function() {
+  const fact = await getFact(FAVORITE_NUM);
+  console.log(fact);
+});
