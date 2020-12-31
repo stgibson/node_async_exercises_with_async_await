4cc2b4661956c34a54c7382648f0fc60bbb2ffa3
Sean Gibson <seanthomasgibson@gmail.com>
2020-12-31 15:22:53 -0800

    Gets description of species for pokemon. Modified pokemon/pokemon.js.

--- pokemon/pokemon.js
+++ pokemon/pokemon.js
@@ -9,18 +9,40 @@ $(async function() {
     let namesAndUrls = resp.data.results;
     
     // get data for 3 pokemon
-    const promises = [];
+    let promises = [];
     for (let i = 0; i < 3; i++) {
       const randNum = Math.floor(Math.random() * NUM_OF_POKEMON);
       promises.push(axios.get(namesAndUrls[randNum].url));
     }
+    let responses = await Promise.all(promises);
+    const names = [];
+    const speciesUrls = [];
+    for (let response of responses) {
+      const pokemon = response.data;
+      names.push(pokemon.name);
+      speciesUrls.push(pokemon.species.url);
+    }
+
+    // get description of species for each of 3 pokemon
+    promises = [];
+    const speciesDescriptions = [];
+    for (let speciesUrl of speciesUrls) {
+      promises.push(axios.get(speciesUrl));
+    }
     responses = await Promise.all(promises);
     for (let response of responses) {
-      console.log(response.data);
+      for (let speciesDescription of response.data.flavor_text_entries) {
+        if (speciesDescription.language.name === "en") {
+          speciesDescriptions.push(speciesDescription.flavor_text);
+          break;
+        }
+      }
     }
+    for (let i = 0; i < 3; i++) {
+      console.log(`${names[i]}: ${speciesDescriptions[i]}`);
+    }    
   }
   catch(e) {
     console.log(e);
-    return;
   }
 })
