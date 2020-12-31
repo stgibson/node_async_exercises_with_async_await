977938c765653e6d9cc984c3b8411ec3a99816e5
Sean Gibson <seanthomasgibson@gmail.com>
2020-12-31 14:40:17 -0800

    Getting data for 3 random pokemon. Modified pokemon/pokemon.js.

--- pokemon/pokemon.js
+++ pokemon/pokemon.js
@@ -2,13 +2,22 @@ const BASE_URL = "https://pokeapi.co/api/v2/";
 const NUM_OF_POKEMON = 1118
 
 $(async function() {
-  // get all pokemon
-  let namesAndUrls;
   try {
-    const resp = 
+    // get all pokemon
+    let resp = 
       await axios.get(`${BASE_URL}pokemon/?limit=${NUM_OF_POKEMON}`);
-    namesAndUrls = resp.data.results;
-    console.log(namesAndUrls);
+    let namesAndUrls = resp.data.results;
+    
+    // get data for 3 pokemon
+    const promises = [];
+    for (let i = 0; i < 3; i++) {
+      const randNum = Math.floor(Math.random() * NUM_OF_POKEMON);
+      promises.push(axios.get(namesAndUrls[randNum].url));
+    }
+    responses = await Promise.all(promises);
+    for (let response of responses) {
+      console.log(response.data);
+    }
   }
   catch(e) {
     console.log(e);
